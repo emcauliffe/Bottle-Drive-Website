a841ee50a3fea9c9a95b988ccfb5e8aefedfa53f
emcauliffe <14607273+emcauliffe@users.noreply.github.com>
2020-05-12 16:53:33 -0400

    Fix test failure where Switch not enclosed by BrowserRouter

--- ui/src/Main.js
+++ ui/src/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // import Home from '../pages/Home';
 import Login from './Login';
@@ -7,17 +7,21 @@ import Register from './Register'
 import PickupRegister from './PickupRegister';
 import ViewDrives from "./ViewDrives";
 import Logout from "./Logout"
+import PickupConfirmation from "./PickupConfirmation";
 
-const Main = () => {
+function Main() {
   return (
-    <Switch> {/* The Switch decides which component to show based on the current URL.*/}
-      {/* <Route exact path='/' component={Home}></Route> */}
-      <Route exact path='/signup' component={Register}></Route>
-      <Route exact path='/login' component={Login}></Route>
-      <Route exact path='/list' component={ViewDrives}></Route>
-      <Route exact path='/logout' component={Logout}></Route>
-      <Route exact path='/(\w{5})' component={PickupRegister}></Route>
-    </Switch>
+    <BrowserRouter>
+      <Switch> {/* The Switch decides which component to show based on the current URL.*/}
+        {/* <Route exact path='/' component={Home}></Route> */}
+        <Route exact path='/signup' component={Register}></Route>
+        <Route exact path='/login' component={Login}></Route>
+        <Route exact path='/list' component={ViewDrives}></Route>
+        <Route exact path='/logout' component={Logout}></Route>
+        <Route exact path='/success' component={PickupConfirmation}></Route>
+        <Route exact path='/(\w{5})' component={PickupRegister}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
